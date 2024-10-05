81401f90f2cd8ac3062f2c9a0572c292b88e25ff
Clement Moleko <molekoclement@gmail.com>
2024-10-05 18:09:24 +0200

    test bloc emits with currently added tasks.

--- test/blocs/task_bloc/task_bloc_test.dart
+++ test/blocs/task_bloc/task_bloc_test.dart
@@ -1,4 +1,6 @@
 import 'package:anothertodo/blocs/task_bloc/task_bloc.dart';
+import 'package:anothertodo/common/enum_priority.dart';
+import 'package:anothertodo/models/task.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
@@ -28,6 +30,27 @@ void main() {
   group('TaskBloc', () {
     test('initial state is correct', () {
       expect(taskBloc.state, equals(TaskState(status: TaskStatus.initial)));
+    });
+
+    group("TaskAdded", () {
+      final addedTask = Task(
+        title: 'Test Task',
+        description: 'Test Description',
+        startDate: DateTime.now(),
+        endDate: DateTime.now(),
+        priority: Priority.low,
+        id: '1',
+      );
+
+      blocTest(
+        'emits [TaskState(status: TaskStatus.loading), TaskState(status: TaskStatus.success, tasks: addedTasks)] when TaskAdded is added',
+        build: () => taskBloc,
+        act: (bloc) => bloc.add(TaskAdded(task: addedTask)),
+        expect: () => [
+          TaskState(status: TaskStatus.loading),
+          TaskState(status: TaskStatus.success, tasks: [addedTask]),
+        ],
+      );
     });
   });
 }
